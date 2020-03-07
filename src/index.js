268d80f8017b7b42fb3155921e5b5de3848c2d5d
andreitol <tolstoi.a@gmail.com>
2020-03-07 18:59:37 +0200

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
-}
+/**
+ *
+ * @param {Number} n
+ */
+module.exports = function reverse(n) {
+    let str = n
+        .toString()
+        .split("")
+        .reverse()
+        .join("");
+    return parseInt(str, 10);
+};
+
+// console.log(reverse(95034));
